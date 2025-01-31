45d0b1a1ae5aa3dc170ff59f85d7640cbb6e2801
Yue Yuan <yyuan8@dt-login03.delta.ncsa.illinois.edu>
2025-01-31 00:24:18 -0600

    Release Lab2

---
+++ lab2/lab2.cu
@@ -0,0 +1,81 @@
+#include <wb.h>
+
+#define wbCheck(stmt)                                                     \
+  do {                                                                    \
+    cudaError_t err = stmt;                                               \
+    if (err != cudaSuccess) {                                             \
+      wbLog(ERROR, "Failed to run stmt ", #stmt);                         \
+      wbLog(ERROR, "Got CUDA error ...  ", cudaGetErrorString(err));      \
+      return -1;                                                          \
+    }                                                                     \
+  } while (0)
+
+
+// Compute C = A * B
+__global__ void matrixMultiply(float *A, float *B, float *C, int numARows,
+                               int numAColumns, int numBRows,
+                               int numBColumns, int numCRows,
+                               int numCColumns)
+{
+  //@@ Implement matrix multiplication kernel here
+}
+
+
+int main(int argc, char **argv) {
+  wbArg_t args;
+  float *hostA; // The A matrix
+  float *hostB; // The B matrix
+  float *hostC; // The output C matrix
+  
+  int numARows;    // number of rows in the matrix A
+  int numAColumns; // number of columns in the matrix A
+  int numBRows;    // number of rows in the matrix B
+  int numBColumns; // number of columns in the matrix B
+  int numCRows;    // number of rows in the matrix C (you have to set this)
+  int numCColumns; // number of columns in the matrix C (you have to set
+                   // this)
+
+  args = wbArg_read(argc, argv);
+
+  //@@ Importing data and creating memory on host
+  hostA = (float *)wbImport(wbArg_getInputFile(args, 0), &numARows,
+                            &numAColumns);
+  hostB = (float *)wbImport(wbArg_getInputFile(args, 1), &numBRows,
+                            &numBColumns);
+  wbLog(TRACE, "The dimensions of A are ", numARows, " x ", numAColumns);
+  wbLog(TRACE, "The dimensions of B are ", numBRows, " x ", numBColumns);
+
+  //@@ Set numCRows and numCColumns
+
+
+  //@@ Allocate the hostC matrix
+
+
+  //@@ Allocate GPU memory here
+
+
+  //@@ Copy memory to the GPU here
+
+
+  //@@ Initialize the grid and block dimensions here
+
+
+  //@@ Launch the GPU Kernel here
+
+  cudaDeviceSynchronize();
+  
+  //@@ Copy the GPU memory back to the CPU here
+
+
+  //@@ Free the GPU memory here
+
+
+  wbSolution(args, hostC, numCRows, numCColumns);
+
+  free(hostA);
+  free(hostB);
+  //@@Free the hostC matrix
+
+  return 0;
+}
+
