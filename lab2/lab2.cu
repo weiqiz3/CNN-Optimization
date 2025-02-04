5ded7877bd47427e9979225f674c876b528da945
weiqiz3 <weiqiz3@illinois.edu>
2025-02-04 16:14:10 -0600

    finish lab2

--- lab2/lab2.cu
+++ lab2/lab2.cu
@@ -1,5 +1,4 @@
 #include <wb.h>
-
 #define wbCheck(stmt)                                                     \
   do {                                                                    \
     cudaError_t err = stmt;                                               \
@@ -9,8 +8,6 @@
       return -1;                                                          \
     }                                                                     \
   } while (0)
-
-
 // Compute C = A * B
 __global__ void matrixMultiply(float *A, float *B, float *C, int numARows,
                                int numAColumns, int numBRows,
@@ -18,15 +15,24 @@ __global__ void matrixMultiply(float *A, float *B, float *C, int numARows,
                                int numCColumns)
 {
   //@@ Implement matrix multiplication kernel here
+  // Calculate the row index of the d_P element and d_M
+  int Row = blockIdx.y * blockDim.y + threadIdx.y;
+  // Calculate the column idenx of d_P and d_N
+  int Col = blockIdx.x * blockDim.x + threadIdx.x;
+  if ((Row < numCRows) && (Col < numCColumns)) {
+    float p = 0;
+    // each thread computes one element of the block sub-matrix
+    for (int k = 0; k < numAColumns; k++) {
+      p += A[Row * numAColumns + k] * B[k * numBColumns + Col];
+    }
+    C[Row * numCColumns + Col] = p;
+  }
 }
-
-
 int main(int argc, char **argv) {
   wbArg_t args;
   float *hostA; // The A matrix
   float *hostB; // The B matrix
   float *hostC; // The output C matrix
-  
   int numARows;    // number of rows in the matrix A
   int numAColumns; // number of columns in the matrix A
   int numBRows;    // number of rows in the matrix B
@@ -34,9 +40,7 @@ int main(int argc, char **argv) {
   int numCRows;    // number of rows in the matrix C (you have to set this)
   int numCColumns; // number of columns in the matrix C (you have to set
                    // this)
-
   args = wbArg_read(argc, argv);
-
   //@@ Importing data and creating memory on host
   hostA = (float *)wbImport(wbArg_getInputFile(args, 0), &numARows,
                             &numAColumns);
@@ -44,38 +48,36 @@ int main(int argc, char **argv) {
                             &numBColumns);
   wbLog(TRACE, "The dimensions of A are ", numARows, " x ", numAColumns);
   wbLog(TRACE, "The dimensions of B are ", numBRows, " x ", numBColumns);
-
   //@@ Set numCRows and numCColumns
-
-
+  numCRows = numARows;
+  numCColumns = numBColumns;
   //@@ Allocate the hostC matrix
-
-
+  hostC = (float *) malloc(numCColumns * numCRows * sizeof(float));
+  wbLog(TRACE, "The dimensions of C are ", numCRows, " x ", numCColumns);
   //@@ Allocate GPU memory here
-
-
+  float *devA, *devB, *devC;
+  cudaMalloc((void **) &devA, numAColumns * numARows * sizeof(float));
+  cudaMalloc((void **) &devB, numBColumns * numBRows * sizeof(float));
+  cudaMalloc((void **) &devC, numCColumns * numCRows * sizeof(float));
   //@@ Copy memory to the GPU here
-
-
+  cudaMemcpy(devA, hostA, numAColumns * numARows * sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(devB, hostB, numBColumns * numBRows * sizeof(float), cudaMemcpyHostToDevice);
   //@@ Initialize the grid and block dimensions here
-
-
+  dim3 DimGrid(ceil((float) numCColumns / 16), ceil((float) numCRows / 16), 1);
+  dim3 DimBlock(16, 16, 1);
   //@@ Launch the GPU Kernel here
-
+  matrixMultiply<<<DimGrid, DimBlock>>>(devA, devB, devC, numARows, numAColumns, numBRows, numBColumns, numCRows, numCColumns);
   cudaDeviceSynchronize();
-  
   //@@ Copy the GPU memory back to the CPU here
-
-
+  cudaMemcpy(hostC, devC, numCColumns * numCRows * sizeof(float), cudaMemcpyDeviceToHost);
   //@@ Free the GPU memory here
-
-
+  cudaFree(devA);
+  cudaFree(devB);
+  cudaFree(devC);
   wbSolution(args, hostC, numCRows, numCColumns);
-
   free(hostA);
   free(hostB);
   //@@Free the hostC matrix
-
+  free(hostC);
   return 0;
 }
-
