77997e7022d08d49edb9d868398629795b501cf2
Yue Yuan <yyuan8@dt-login01.delta.ncsa.illinois.edu>
2025-03-07 13:42:49 -0600

    Release Lab 5

---
+++ lab5/lab5.cu
@@ -0,0 +1,87 @@
+// MP5 Reduction
+// Input: A num list of length n
+// Output: Sum of the list = list[0] + list[1] + ... + list[n-1];
+
+#include <wb.h>
+
+#define BLOCK_SIZE 512 //@@ This value is not fixed and you can adjust it according to the situation
+
+#define wbCheck(stmt)                                                     \
+  do {                                                                    \
+    cudaError_t err = stmt;                                               \
+    if (err != cudaSuccess) {                                             \
+      wbLog(ERROR, "Failed to run stmt ", #stmt);                         \
+      wbLog(ERROR, "Got CUDA error ...  ", cudaGetErrorString(err));      \
+      return -1;                                                          \
+    }                                                                     \
+  } while (0)
+  
+__global__ void total(float *input, float *output, int len) {
+  //@@ Load a segment of the input vector into shared memory
+  //@@ Traverse the reduction tree
+  //@@ Write the computed sum of the block to the output vector at the correct index
+}
+
+int main(int argc, char **argv) {
+  int ii;
+  wbArg_t args;
+  float *hostInput;  // The input 1D list
+  float *hostOutput; // The output list
+  //@@ Initialize device input and output pointers
+
+  int numInputElements;  // number of elements in the input list
+  int numOutputElements; // number of elements in the output list
+
+  args = wbArg_read(argc, argv);
+
+  //Import data and create memory on host
+  hostInput =
+      (float *)wbImport(wbArg_getInputFile(args, 0), &numInputElements);
+
+  numOutputElements = numInputElements / (BLOCK_SIZE << 1);
+  if (numInputElements % (BLOCK_SIZE << 1)) {
+    numOutputElements++;
+  }
+  hostOutput = (float *)malloc(numOutputElements * sizeof(float));
+
+  // The number of input elements in the input is numInputElements
+  // The number of output elements in the input is numOutputElements
+
+  //@@ Allocate GPU memory
+
+
+  //@@ Copy input memory to the GPU
+
+
+  //@@ Initialize the grid and block dimensions here
+
+
+  //@@ Launch the GPU Kernel and perform CUDA computation
+
+  
+  cudaDeviceSynchronize();  
+  //@@ Copy the GPU output memory back to the CPU
+
+  
+  /********************************************************************
+   * Reduce output vector on the host
+   * NOTE: One could also perform the reduction of the output vector
+   * recursively and support any size input. 
+   * For simplicity, we do not require that for this lab.
+   ********************************************************************/
+  for (ii = 1; ii < numOutputElements; ii++) {
+    hostOutput[0] += hostOutput[ii];
+  }
+
+  //@@ Free the GPU memory
+
+
+
+  wbSolution(args, hostOutput, 1);
+
+  free(hostInput);
+  free(hostOutput);
+
+  return 0;
+}
+
